4712cfefb217827c2be12d4d683db167c89e7ed4
MichaelBurak <michaelrburak@gmail.com>
2019-02-28 11:33:56 -0500

    adding custom vuetify theme

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,7 +1,17 @@
-import Vue from 'vue'
-import Vuetify from 'vuetify/lib'
-import 'vuetify/src/stylus/app.styl'
+import Vue from "vue";
+import Vuetify from "vuetify/lib";
+import "vuetify/src/stylus/app.styl";
+
+import colors from "vuetify/es5/util/colors";
 
 Vue.use(Vuetify, {
-  iconfont: 'md',
-})
+  iconfont: "md",
+  theme: {
+    primary: colors.blueGrey.lighten5,
+    secondary: colors.teal.darken3,
+    accent: colors.deepOrange.darken3
+  },
+  options: {
+    customProperties: true
+  }
+});
